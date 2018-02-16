9d7b869f8399182654c329e522b3213439c47d56
Shelbi Schollenberger <sschollenberger6@gmail.com>
2018-02-15 22:02:43 -0700

    comments are working and adding to the database

--- comments.inc.php
+++ comments.inc.php
@@ -1,7 +1,12 @@
 <?php
 
-function setComment() {
+function setComment($connection) {
   if (isset($_POST['commentSubmit'])) {
-    
+    $uid = $_POST['uid'];
+    $date = $_POST['date'];
+    $message = $_POST['message'];
+
+    $sql = "INSERT INTO comments (uid, date, message) VALUES ('$uid', '$date', '$message')";
+    $result = mysqli_query($connection, $sql);
   }
 }
