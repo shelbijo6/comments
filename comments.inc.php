28dff87e4fb55fcdc8e1788c0a75efbfbf645e13
Shelbi Schollenberger <sschollenberger6@gmail.com>
2018-02-21 20:24:44 -0700

    added a delete button

--- comments.inc.php
+++ comments.inc.php
@@ -15,11 +15,23 @@ function getComment($connection) {
   $sql = "SELECT * FROM comments";
   $result = mysqli_query($connection, $sql);
   while ($row = $result->fetch_assoc()) {
-    echo "<div class''comment-box><p>";
+    echo "<div class='comment-box'><p>";
       echo $row['uid']."<br>";
       echo $row['date']."<br>";
-      echo nl2br($row['message']);
-    echo "</p></div>";
+      echo nl2br($row['MESSAGE']);
+    echo "</p>
+            <form class='delete-form' method='POST' action='".deleteComment($connection)."'>
+              <input type='hidden' name='cid' value='".$row['cid']."'>
+              <button type='submit' name='commentDelete'>Delete</button>
+            </form>
+            <form class='edit-form' method='POST' action='editcomment.php'>
+              <input type='hidden' name='cid' value='".$row['cid']."'>
+              <input type='hidden' name='uid' value='".$row['uid']."'>
+              <input type='hidden' name='date' value='".$row['date']."'>
+              <input type='hidden' name='MESSAGE' value='".$row['MESSAGE']."'>
+              <button>Edit</button>
+            </form>
+          </div>";
   }
 }
 
@@ -31,6 +43,16 @@ function editComment($connection) {
     $message = $_POST['message'];
 
     $sql = "UPDATE comments SET message='$message' WHERE cid='$cid'";
+    $result = mysqli_query($connection, $sql);
+    header("Location: index.php");
+  }
+}
+
+function deleteComment($connection) {
+  if (isset($_POST['commentDelete'])) {
+    $cid = $_POST['cid'];
+
+    $sql = "DELETE FROM comments WHERE cid='$cid'";
     $result = mysqli_query($connection, $sql);
     header("Location: index.php");
   }
